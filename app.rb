186011d1956328f72c6949e29fbc205b0b8ff8d2
AHMED AFANDY <eng.computer1@yahoo.com>
2020-05-16 19:42:09 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,9 +1,27 @@
+require 'pry'
 require './environment'
 
 module FormsLab
   class App < Sinatra::Base
 
-    # code other routes/actions here
+    get '/' do
+      erb :root
+    end
 
+    get '/new' do
+      erb :'pirates/new'
+    end
+
+    post '/pirates' do
+      @pirate=Pirate.new(pirate_h:params[:pirate])
+      params[:pirate][:ships].each do |ship|
+         @ships=Ship.new(ship_h:ship)
+      end
+      erb :'pirates/show'
+    end
   end
 end
+
+
+#pirates[pirate][name]      pirates[pirate][weight]  pirates[pirate][height]  pirates[pirate][ships][][name] pirates[pirate][ships][][booty]
+#pirates[pirate][ships][][type]
